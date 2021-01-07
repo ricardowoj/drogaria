6eb782958c623264aee88f20dc6fb59024295194
ricardowoj <ricardowoj@hotmail.com>
2021-01-07 19:55:34 -0300

    Adicionado Fabricante.java

---
+++ Drogaria/src/main/java/br/com/ricardow/drogaria/domain/Fabricante.java
@@ -0,0 +1,36 @@
+package br.com.ricardow.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@SuppressWarnings("serial")
+@Entity
+public class Fabricante extends GenericDomain {
+	@Column(length = 50, nullable = false)
+	private String razaoSocial;
+	
+	@Column(length = 12, nullable = false)
+	private String cnpj;
+	
+	@Column(length = 500, nullable = false)
+	private String descricao;
+	
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
