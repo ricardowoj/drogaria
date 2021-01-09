02de177d7c2b48afc62918da840c285bfa6d27c0
ricardowoj <ricardowoj@hotmail.com>
2021-01-09 10:10:22 -0300

    Criado método listar no EstadoDAOTest

--- Drogaria/src/test/java/br/com/ricardow/drogaria/dao/EstadoDAOTest.java
+++ Drogaria/src/test/java/br/com/ricardow/drogaria/dao/EstadoDAOTest.java
@@ -1,11 +1,15 @@
 package br.com.ricardow.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.ricardow.drogaria.domain.Estado;
 
 public class EstadoDAOTest {
 	@Test
+	@Ignore
 	public void salvar() {
 		Estado estado = new Estado();
 		estado.setNome("São Paulo");
@@ -13,5 +17,17 @@ public class EstadoDAOTest {
 		
 		EstadoDAO estadoDAO = new EstadoDAO();
 		estadoDAO.salvar(estado);
+	}
+	
+	@Test
+	public void listar() {
+		EstadoDAO estadoDAO = new EstadoDAO();
+		List<Estado> resultado = estadoDAO.listar();
+		
+		System.out.println("Total de registro: " + resultado.size());
+		
+		for(Estado estado : resultado) {
+			System.out.println(estado.getSigla() + " - " + estado.getNome());
+		}
 	}
 }
