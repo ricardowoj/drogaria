f04e6cf25ad18341c25656935dab079ccb230057
ricardowoj <ricardowoj@hotmail.com>
2021-01-09 11:07:19 -0300

    Adicionado o método excluir no GenericDAO.java

--- Drogaria/src/main/java/br/com/ricardow/drogaria/dao/GenericDAO.java
+++ Drogaria/src/main/java/br/com/ricardow/drogaria/dao/GenericDAO.java
@@ -71,4 +71,24 @@ public class GenericDAO<Entidade> {
 			sessao.close();
 		}
 	}
+	
+	public void excluir(Entidade entidade) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		Transaction transacao = null;
+		
+		try {
+			transacao = sessao.beginTransaction();
+			sessao.delete(entidade);
+			transacao.commit();
+		}
+		catch(RuntimeException e) { 
+			if (transacao != null) {
+				transacao.rollback();
+			}
+			throw e;
+		}
+		finally {
+			sessao.close();
+		}
+	}
 }
