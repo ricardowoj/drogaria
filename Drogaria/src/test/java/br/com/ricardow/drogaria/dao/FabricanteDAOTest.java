6157cbf788d775e2c7f4add07ae89ec27c99e884
ricardowoj <ricardowoj@hotmail.com>
2021-01-09 10:18:39 -0300

    Adicionado método listar no FabricanteDAOTest.

--- Drogaria/src/test/java/br/com/ricardow/drogaria/dao/FabricanteDAOTest.java
+++ Drogaria/src/test/java/br/com/ricardow/drogaria/dao/FabricanteDAOTest.java
@@ -1,11 +1,15 @@
 package br.com.ricardow.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.ricardow.drogaria.domain.Fabricante;
 
 public class FabricanteDAOTest {
 	@Test
+	@Ignore
 	public void salvar() {
 		Fabricante fabricante = new Fabricante();
 		fabricante.setRazaoSocial("SOFAPE FABRICANTE DE FILTROS LTDA");
@@ -14,5 +18,17 @@ public class FabricanteDAOTest {
 		
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.salvar(fabricante);
+	}
+	
+	@Test
+	public void listar() {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> resultado = fabricanteDAO.listar();
+		
+		System.out.println("Total de registro: " + resultado.size());
+		
+		for(Fabricante fabricante : resultado) {
+			System.out.println(fabricante.getRazaoSocial() + " - " + fabricante.getCnpj() + " - " + fabricante.getDescricao());
+		}
 	}
 }
