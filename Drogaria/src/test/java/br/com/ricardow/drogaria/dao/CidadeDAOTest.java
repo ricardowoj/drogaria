d49bfbaf3e6a9cf502cb1c10295ebb8d0bc9a168
ricardowoj <ricardowoj@hotmail.com>
2021-01-10 11:30:12 -0300

    Adicionado CidadeDAOTest.java

--- Drogaria/src/test/java/br/com/ricardow/drogaria/dao/CidadeDAOTest.java
+++ Drogaria/src/test/java/br/com/ricardow/drogaria/dao/CidadeDAOTest.java
@@ -1,5 +1,7 @@
 package br.com.ricardow.drogaria.dao;
 
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -14,10 +16,90 @@ public class CidadeDAOTest {
 		Estado estado = estadoDAO.buscar(21L);
 		
 		Cidade cidade = new Cidade();
-		cidade.setNome("São Paulo");
+		cidade.setNome("Osasco");
 		cidade.setEstado(estado);
 		
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		cidadeDAO.salvar(cidade);
+	}
+	
+	@Test
+	@Ignore
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+		
+		for(Cidade cidade : resultado) {
+			System.out.println(
+					"Nome da cidade: " + cidade.getNome() 
+					+ "\n" 
+					+ "Nome do estado: " + cidade.getEstado().getNome());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void buscar() {
+		Long codigo = 23L;
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+		
+		if (cidade == null) {
+			System.out.println("Nenhum registro encontrado.");
+		}
+		else {
+			System.out.println(
+					"Nome da cidade: " + cidade.getNome() 
+					+ "\n" 
+					+ "Nome do estado: " + cidade.getEstado().getNome());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void excluir() {
+		Long codigo = 23L;
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+		
+		if(cidade == null) {
+			System.out.println("Nenhum registro encontrado.");
+		}
+		else {
+			cidadeDAO.excluir(cidade);
+			System.out.println("Registro removido:");
+			System.out.println("Nome da cidade: " + cidade.getNome());
+		}
+	}
+	
+	@Test
+	public void editar() {
+		Long codigoEstado = 22L;
+		Long codigoCidade = 24L;
+		
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigoEstado);
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigoCidade);
+		
+		if(estado == null) {
+			System.out.println("Nenhum estado localizado");
+		}
+		else if (cidade == null) {
+			System.out.println("Nenhuma cidade localizada");
+		}
+		else {
+			cidade.setNome("Vitória");
+			cidade.setEstado(estado);
+			cidadeDAO.editar(cidade);
+			System.out.println("Registro editado:");
+			System.out.println(
+					"Nome da cidade: " + cidade.getNome() 
+					+ "\n" 
+					+ "Nome do estado: " + cidade.getEstado().getNome());
+		}
 	}
 }
